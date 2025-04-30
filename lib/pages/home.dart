915d89101ce7dcb96b022d07c759812615fa96f0
Mithun M <mithun@Mithuns-MacBook-Air.local>
2025-05-01 03:30:23 +0530

    web Application code

---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:demo/utiles/constant.dart';
+import 'package:demo/widgets/navbar.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(child: Column(children: [Navbar()])),
+      ),
+    );
+  }
+}
